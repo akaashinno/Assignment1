151eeb2bb85c0c5d9b8c0b509032d0c1cdfed8c4
akaashinno <akash.tomer@innotechteam.in>
2023-08-07 12:35:58 +0530

    add error handler

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -1,13 +1,20 @@
 const Role = require("../models/roles");
 const sequelize = require("../database/db");
+
 module.exports = {
   addRole: async (req, res) => {
-    const { name, description } = req.body;
-    const createRole = await Role.create({
-      name: name,
-      description: description,
-    });
-    createRole.save();
-    res.status(200).send({ message: "data saved successfully" });
+    try {
+      const { name, description } = req.body;
+      const createRole = await Role.create({
+        name: name,
+        description: description,
+      });
+      // throw err;
+      createRole.save();
+      res.status(200).send({ message: "data saved successfully" });
+    } catch (err) {
+      console.log("Error occurred:", err);
+      res.status(500).send({ message: "Internal server error" });
+    }
   },
 };
