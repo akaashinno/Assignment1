93252f64574aef7c45baef78cd4fa00c7a13697f
akaashinno <akash.tomer@innotechteam.in>
2023-08-07 18:22:13 +0530

    completed Q1b,c

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { addRole } = require("../controllers/roleController");
+const {
+  addRole,
+  deleteRole,
+  editRole,
+  listRole,
+  singleRole,
+} = require("../controllers/roleController");
 
 router.post("/add", addRole);
+router.delete("/delete/:id", deleteRole);
+router.put("/edit/:id", editRole);
+router.get("/list", listRole);
+router.get("/:id", singleRole);
 
 module.exports = router;
